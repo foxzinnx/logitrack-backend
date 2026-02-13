7361965778cdc7b86b4116223ad85c8b26a176e7
foxz1n <bryangomes16624@gmail.com>
2026-02-12 23:18:33 -0300

    feat: package entity

---
+++ src/domain/entities/Package.ts
@@ -0,0 +1,14 @@
+export enum PackageStatus{
+    PENDING = 'PENDING',
+    AWAITING_PICKUP = 'AWAITING_PICKUP',
+    PICKED_UP = 'PICKED_UP',
+    IN_TRANSIT = 'IN_TRANSIT',
+    DELIVERED = 'DELIVERED',
+    RETURNED = 'RETURNED'
+}
+
+export class Package{
+    private constructor(
+        
+    ){}
+}
