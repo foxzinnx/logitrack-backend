7cab6ef6744fb6a1b207f265b6ffaf3f186b1804
foxz1n <bryangomes16624@gmail.com>
2026-02-14 21:24:22 -0300

    feat: GetPackageDetailsUseCase

---
+++ src/application/use-cases/package/GetPackageDetailsUseCase.ts
@@ -0,0 +1,42 @@
+import type { PackageResponseDTO } from "@/application/dtos/PackageResponseDTO.js";
+import type { Package } from "@/domain/entities/Package.js";
+import type { IPackageRepository } from "@/domain/repositories/IPackageRepository.js";
+import { UUID } from "@/domain/value-objects/UUID.js";
+
+export class GetPackageDetailsUseCase{
+    constructor(private packageRepository: IPackageRepository){}
+
+    async execute(packageId: string): Promise<PackageResponseDTO> {
+        const id = UUID.create(packageId);
+        const pkg = await this.packageRepository.findById(id);
+
+        if(!pkg){
+            throw new Error('Package not found')
+        }
+
+        return this.toDTO(pkg);
+    }
+
+    private toDTO(pkg: Package): PackageResponseDTO {
+        return {
+            id: pkg.getId().getValue(),
+            recipientName: pkg.getRecipientName(),
+            recipientPhone: pkg.getRecipientPhone(),
+            address: {
+                street: pkg.getDeliveryAddress().getStreet(),
+                number: pkg.getDeliveryAddress().getNumber(),
+                complement: pkg.getDeliveryAddress().getComplement(),
+                neighborhood: pkg.getDeliveryAddress().getNeighborhood(),
+                city: pkg.getDeliveryAddress().getCity(),
+                state: pkg.getDeliveryAddress().getState(),
+                zipCode: pkg.getDeliveryAddress().getZipCode()
+            },
+            status: pkg.getStatus(),
+            delivererId: pkg.getDelivererId()?.getValue() || null,
+            deliveryPhotoUrl: pkg.getDeliveryPhotoUrl(),
+            deliveredAt: pkg.getDeliveredAt(),
+            createdAt: pkg.getCreatedAt(),
+            updatedAt: pkg.getUpdatedAt()
+        };
+    }
+}
