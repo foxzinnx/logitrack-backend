7c033602c293254c39c4d95f711be0d98bbeb970
foxz1n <bryangomes16624@gmail.com>
2026-02-18 14:24:48 -0300

    feat: PackageError

---
+++ src/domain/errors/PackageErrors.ts
@@ -0,0 +1,43 @@
+import { DomainError } from "./DomainError.js";
+
+export class PackageNotFoundError extends DomainError {
+    constructor(packageId?: string){
+        super(packageId ? `Package not found: ${packageId}` : 'Package not found');
+    }
+}
+
+export class PackageAlreadyAssignedError extends DomainError {
+    constructor(){
+        super(`Package is already assigned to a driver`);
+    }
+}
+
+export class PackageCannotBeAssignedError extends DomainError {
+    constructor(currentStatus: string){
+        super(`Package cannot be assigned in current status: ${currentStatus}`);
+    }
+}
+
+export class PackageCannotBeDeliveredError extends DomainError {
+    constructor(reason: string){
+        super(`Package cannot be delivered: ${reason}`);
+    }
+}
+
+export class DeliveryPhotoRequiredError extends DomainError{
+    constructor(){
+        super('Delivery photo is required to mark package as delivered');
+    }
+}
+
+export class PackageNotAssigned extends DomainError{
+    constructor(){
+        super('Package must be assigned to a deliverer before delivery');
+    }
+}
+
+export class InvalidStatusTransitionError extends DomainError {
+    constructor(from: string, to: string){
+        super(`Cannot transition package status from ${from} to ${to}`);
+    }
+}
